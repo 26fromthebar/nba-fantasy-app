36ca34e0ec3b7893e5157120782abd8b983f35dc
26fromthebar <kostas.gelinio@gmail.com>
2021-11-16 20:33:18 +0200

    min functionality for heroku

--- src/routers/playerRouter.js
+++ src/routers/playerRouter.js
@@ -40,30 +40,4 @@ router.post('/squads', async (req, res) => {
   }
 });
 
-// Get admin add-player page
-router.get('/add-player', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public', 'add_player.html'));
-});
-
-// Add player to DB
-router.post('/add-player', async (req, res) => {
-  try {
-    const player = new Player({
-      name: req.body.name,
-      team: req.body.team.toUpperCase(),
-      position: req.body.position.toUpperCase(),
-      price: parseFloat(req.body.price),
-      avPoints: parseFloat(req.body.avPoints),
-      nextGames: parseFloat(req.body.nextGames),
-    });
-    await player.save();
-    res.status(200).send({
-      message: `Player '${player.name}' has been added to database`,
-      player,
-    });
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-
 module.exports = router;
